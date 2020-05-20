b748f66afd965f4a2bf173d19979073ff7a51ac1
McPqndq <kellybob3737@gmail.com>
2020-05-19 23:16:25 -0500

    honestly idk

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import utils from './lib/utils/common.js';
 import deflate from './lib/deflate.js';
 import inflate from './lib/inflate.js';
 import constants from './lib/zlib/constants.js';
-
-export default {...deflate, ...inflate, ...constants};
+const pako = {...deflate, ...inflate, ...constants};
+export default pako;
